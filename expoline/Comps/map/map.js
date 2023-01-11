4b4df62a70cad03a0e62a37744235edf122c2f06
SofiaBCIT <sofia.sirianni@outlook.comm>
2023-01-11 10:20:15 -0800

    error fix

---
+++ expoline/Comps/map/map.js
@@ -0,0 +1,36 @@
+import 'leaflet/dist/leaflet.css'
+import 'leaflet-defaulticon-compatibility'
+import 'leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility'
+
+import style from '../../styles/Home.module.css'
+
+import { MapContainer, TileLayer, useMap, Marker, Popup, Polyline, CircleMarker } from 'react-leaflet'
+
+export default function Map (){
+
+    const waterfront = [49.28594, -123.11129]; 
+    
+
+
+    return (
+        <MapContainer className={style.map} center={waterfront} zoom={14} scrollWheelZoom={true}>
+            <TileLayer
+                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+
+            <Marker position={waterfront}>
+                <Popup>
+                Skytrain. <br/> Waterfront.
+                </Popup>
+            </Marker>
+
+            <Marker position={[49.2856, -123.1198]}>
+                <Popup>
+                Skytrain. <br/> Waterfront.
+                </Popup>
+            </Marker>
+
+        </MapContainer>
+    )
+}
