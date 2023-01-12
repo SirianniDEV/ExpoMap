5da8dac71f50d46d5ec313e1e07299ed68d14734
SofiaBCIT <sofia.sirianni@outlook.comm>
2023-01-12 10:34:06 -0800

    update

--- expoline/pages/index.js
+++ expoline/pages/index.js
@@ -2,7 +2,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import styles from '../styles/Home.module.css'
-import Map from '../Comps/Map/'
+import Map from '../Comps/map/'
 import Info from '../Comps/Info/info'
 import Footer from '../Comps/Footer/footer'
 
